557f80d4fc630374a057e054c81b095556bc27b7
haosu <me@haosu.org>
2014-05-27 00:00:24 -0700

    Add map info for all of the videos

--- javascript/videos.js
+++ javascript/videos.js
@@ -1,172 +1,446 @@
+LAT = "35.6935607";
+LNG = "139.7649763";
 Travel.VideoData = {
   "videos": [
     {
-      "video_path": "./videos/IMG_2597.MOV"
+      "video_path": "./videos/IMG_2597.MOV",
+      "title": "The trip in",
+      "location": {
+        "lat": "35.7366466",
+        "lng": "139.826058"
+      },
+      "story": {
+      }
     },
     {
-      "video_path": "./videos/IMG_2621.MOV"
+      "video_path": "./videos/IMG_2621.MOV",
+      "title": "The trip in",
+      "zoom": "16",
+      "location": {
+        "lat": "35.7121182",
+        "lng": "139.7954923"
+      },
+      "story": {
+      }
     },
     {
-      "video_path": "./videos/IMG_2626.MOV"
+      "video_path": "./videos/IMG_2626.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.715006",
+        "lng": "139.797408",
+      }
     },
     {
-      "video_path": "./videos/IMG_2627.MOV"
+      "video_path": "./videos/IMG_2627.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.715006",
+        "lng": "139.797408",
+      }
     },
     {
-      "video_path": "./videos/IMG_2631.MOV"
+      "video_path": "./videos/IMG_2631.MOV",
+      "location": {
+        "lat": LAT,
+        "lng": LNG
+      }
     },
     {
-      "video_path": "./videos/IMG_2645.MOV"
+      "video_path": "./videos/IMG_2645.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.6943033",
+        "lng": "139.7418606"
+      }
     },
     {
-      "video_path": "./videos/IMG_2659.MOV"
+      "video_path": "./videos/IMG_2659.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.6943033",
+        "lng": "139.7418606"
+      }
     },
     {
-      "video_path": "./videos/IMG_2696.MOV"
+      "video_path": "./videos/IMG_2696.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.692972",
+        "lng": "139.699444"
+      }
     },
     {
-      "video_path": "./videos/IMG_2698.MOV"
+      "video_path": "./videos/IMG_2698.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.692972",
+        "lng": "139.699444"
+      }
     },
     {
-      "video_path": "./videos/IMG_2701.MOV"
+      "video_path": "./videos/IMG_2701.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.693639",
+        "lng": "139.700644"
+      }
     },
     {
-      "video_path": "./videos/IMG_2704.MOV"
+      "video_path": "./videos/IMG_2704.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6987533",
+        "lng": "139.7050722"
+      }
     },
     {
-      "video_path": "./videos/IMG_2707.MOV"
+      "video_path": "./videos/IMG_2707.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6987533",
+        "lng": "139.7050722"
+      }
     },
     {
-      "video_path": "./videos/IMG_2708.MOV"
+      "video_path": "./videos/IMG_2708.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6987533",
+        "lng": "139.7050722"
+      }
     },
     {
-      "video_path": "./videos/IMG_2711.MOV"
+      "video_path": "./videos/IMG_2711.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.69379",
+        "lng": "139.704203"
+      }
     },
     {
-      "video_path": "./videos/IMG_2717.MOV"
+      "video_path": "./videos/IMG_2717.MOV",
+      "location": {
+        "lat": "35.694347",
+        "lng": "139.702743"
+      }
     },
     {
-      "video_path": "./videos/IMG_2728.MOV"
+      "video_path": "./videos/IMG_2728.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.692972",
+        "lng": "139.699444"
+      }
     },
     {
-      "video_path": "./videos/IMG_2730.MOV"
+      "video_path": "./videos/IMG_2730.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.692972",
+        "lng": "139.699444"
+      }
     },
     {
-      "video_path": "./videos/IMG_2733.MOV"
+      "video_path": "./videos/IMG_2733.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.667864",
+        "lng": "139.705582"
+      }
     },
     {
-      "video_path": "./videos/IMG_2742.MOV"
+      "video_path": "./videos/IMG_2742.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.6675158",
+        "lng": "139.7067619"
+      }
     },
     {
-      "video_path": "./videos/IMG_2747.MOV"
+      "video_path": "./videos/IMG_2747.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.6714818",
+        "lng": "139.7030811"
+      }
     },
     {
-      "video_path": "./videos/IMG_2777.MOV"
+      "video_path": "./videos/IMG_2777.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.669249",
+        "lng": "139.705809"
+      }
     },
     {
-      "video_path": "./videos/IMG_2778.MOV"
+      "video_path": "./videos/IMG_2778.MOV",
+      "location": {
+        "lat": "35.6701061",
+        "lng": "139.7048531"
+      }
     },
     {
-      "video_path": "./videos/IMG_2782.MOV"
+      "video_path": "./videos/IMG_2782.MOV",
+      "location": {
+        "lat": "35.6701061",
+        "lng": "139.7048531"
+      }
     },
     {
-      "video_path": "./videos/IMG_2784.MOV"
+      "video_path": "./videos/IMG_2784.MOV",
+      "location": {
+        "lat": "35.6701061",
+        "lng": "139.7048531"
+      }
     },
     {
-      "video_path": "./videos/IMG_2796.MOV"
+      "video_path": "./videos/IMG_2796.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.669765",
+        "lng": "139.703190"
+      }
     },
     {
-      "video_path": "./videos/IMG_2797.MOV"
+      "video_path": "./videos/IMG_2797.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.669765",
+        "lng": "139.703190"
+      }
     },
     {
-      "video_path": "./videos/IMG_2804.MOV"
+      "video_path": "./videos/IMG_2804.MOV",
+      "location": {
+        "lat": "35.694347",
+        "lng": "139.702743"
+      }
     },
     {
-      "video_path": "./videos/IMG_2828.MOV"
+      "video_path": "./videos/IMG_2828.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.694347",
+        "lng": "139.702743"
+      }
     },
     {
-      "video_path": "./videos/IMG_2831.MOV"
+      "video_path": "./videos/IMG_2831.MOV",
+      "location": {
+        "lat": "35.694347",
+        "lng": "139.702743"
+      }
     },
     {
-      "video_path": "./videos/IMG_2834.MOV"
+      "video_path": "./videos/IMG_2834.MOV",
+      "location": {
+        "lat": "35.694347",
+        "lng": "139.702743"
+      }
     },
     {
-      "video_path": "./videos/IMG_2846.MOV"
+      "video_path": "./videos/IMG_2846.MOV",
+      "zoom": "17",
+      "location": {
+        "lat": "35.693917",
+        "lng": "139.704663"
+      }
     },
     {
-      "video_path": "./videos/IMG_2848.MOV"
+      "video_path": "./videos/IMG_2848.MOV",
+      "zoom": "17",
+      "location": {
+        "lat": "35.693917",
+        "lng": "139.704663"
+      }
     },
     {
-      "video_path": "./videos/IMG_2861.MOV"
+      "video_path": "./videos/IMG_2861.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.659501",
+        "lng": "139.700472"
+      }
     },
     {
-      "video_path": "./videos/IMG_2868.MOV"
+      "video_path": "./videos/IMG_2868.MOV",
+      "location": {
+        "lat": "35.6626726",
+        "lng": "139.7023991"
+      }
     },
     {
-      "video_path": "./videos/IMG_2877.MOV"
+      "video_path": "./videos/IMG_2877.MOV",
+      "location": {
+        "lat": LAT,
+        "lng": LNG
+      }
     },
     {
-      "video_path": "./videos/IMG_2881.MOV"
+      "video_path": "./videos/IMG_2881.MOV",
+      "location": {
+        "lat": LAT,
+        "lng": LNG
+      }
     },
     {
-      "video_path": "./videos/IMG_2891.MOV"
+      "video_path": "./videos/IMG_2891.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.705038",
+        "lng": "139.792228"
+      }
     },
     {
-      "video_path": "./videos/IMG_2893.MOV"
+      "video_path": "./videos/IMG_2893.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.667660",
+        "lng": "139.761252"
+      }
     },
     {
-      "video_path": "./videos/IMG_2939.MOV"
+      "video_path": "./videos/IMG_2939.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6632046",
+        "lng": "139.732822"
+      }
     },
     {
-      "video_path": "./videos/IMG_2947.MOV"
+      "video_path": "./videos/IMG_2947.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6632046",
+        "lng": "139.732822"
+      }
     },
     {
-      "video_path": "./videos/IMG_2948.MOV"
+      "video_path": "./videos/IMG_2948.MOV",
+      "zoom": "15",
+      "location": {
+        "lat": "35.6632046",
+        "lng": "139.732822"
+      }
     },
     {
-      "video_path": "./videos/IMG_2961.MOV"
+      "video_path": "./videos/IMG_2961.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.663410",
+        "lng": "139.730961"
+      }
     },
     {
-      "video_path": "./videos/IMG_2982.MOV"
+      "video_path": "./videos/IMG_2982.MOV",
+      "zoom": "17",
+      "location": {
+        "lat": "35.711088",
+        "lng": "139.711215"
+      }
     },
     {
-      "video_path": "./videos/IMG_2987.MOV"
+      "video_path": "./videos/IMG_2987.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.710736",
+        "lng": "139.712541"
+      }
     },
     {
-      "video_path": "./videos/IMG_2995.MOV"
+      "video_path": "./videos/IMG_2995.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.710736",
+        "lng": "139.712541"
+      }
     },
     {
-      "video_path": "./videos/IMG_3001.MOV"
+      "video_path": "./videos/IMG_3001.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.710736",
+        "lng": "139.712541"
+      }
     },
     {
-      "video_path": "./videos/IMG_3007.MOV"
+      "video_path": "./videos/IMG_3007.MOV",
+      "zoom": "16",
+      "location": {
+        "lat": "35.6885373",
+        "lng": "139.7003307"
+      }
     },
     {
-      "video_path": "./videos/IMG_3025.MOV"
+      "video_path": "./videos/IMG_3025.MOV",
+      "zoom": "16",
+      "location": {
+        "lat": "35.6885373",
+        "lng": "139.7003307"
+      }
     },
     {
-      "video_path": "./videos/IMG_3044.MOV"
+      "video_path": "./videos/IMG_3044.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3054.MOV"
+      "video_path": "./videos/IMG_3054.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3085.MOV"
+      "video_path": "./videos/IMG_3085.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3086.MOV"
+      "video_path": "./videos/IMG_3086.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3097.MOV"
+      "video_path": "./videos/IMG_3097.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3099.MOV"
+      "video_path": "./videos/IMG_3099.MOV",
+      "zoom": "18",
+      "location": {
+        "lat": "35.665751",
+        "lng": "139.770289"
+      }
     },
     {
-      "video_path": "./videos/IMG_3129.MOV"
+      "video_path": "./videos/IMG_3129.MOV",
+      "location": {
+        "lat": LAT,
+        "lng": LNG
+      }
     },
     {
-      "video_path": "./videos/IMG_3130.MOV"
+      "video_path": "./videos/IMG_3130.MOV",
+      "location": {
+        "lat": LAT,
+        "lng": LNG
+      }
     }
   ]
 }
